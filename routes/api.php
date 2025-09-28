832d038f6e055781a2ae6e2a2baa47c6954d0738
Bruno Alvarado <balvarado@mobydigital.com>
2025-09-27 22:32:30 -0300

    merge sanctum

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Api\CommentController;
 use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route; 
 
 Route::get('/user', function (Request $request) {
     return $request->user();
